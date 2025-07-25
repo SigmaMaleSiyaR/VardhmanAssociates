c29d18a06fc787d587ca0029d3b659b12820f7ea
spamSharma <llpunimentsolution@gmail.com>
2025-07-25 09:38:10 +0530

    blog admin working completed almost

---
+++ admin/imagekit-config.js
@@ -0,0 +1,15 @@
+// ImageKit configuration for use in admin scripts
+
+const IMAGEKIT_PUBLIC_KEY = "public_C5juPBxFMNI+aIo/PkxOrNXPtFM=";
+const IMAGEKIT_PRIVATE_KEY = "private_+4cDAMAoXCWHmPL6WMMKiq5ZI+g=";
+const IMAGEKIT_AUTH_ENDPOINT = "https://vablogs.vercel.app/api/auth";
+const IMAGEKIT_THUMBNAIL_FOLDER = "/blog-thumbnails";
+const IMAGEKIT_CONTENT_FOLDER = "/blog-content";
+
+export {
+  IMAGEKIT_PUBLIC_KEY,
+  IMAGEKIT_PRIVATE_KEY,
+  IMAGEKIT_AUTH_ENDPOINT,
+  IMAGEKIT_THUMBNAIL_FOLDER,
+  IMAGEKIT_CONTENT_FOLDER
+}; 
